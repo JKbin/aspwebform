53aaf1331036bbd6efb4db3cf401e56147aaea1c
JangKookBin <a85479094@gmail.com>
2020-11-09 23:35:20 +0900

    Mobile Login complete

---
+++ Mobile/Mlogin.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Mlogin.aspx.cs" Inherits="ProjectWebForm.Models.Mlogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="../css/Mlogin.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" class="box" method="post">
+        <h1>Login</h1>
+        <asp:TextBox ID="txtCode"  runat="server" type ="text" name="" placeholder="Username"></asp:TextBox>
+        <asp:TextBox ID="txtpwd"  runat="server" type ="Password" name="" placeholder="Password"></asp:TextBox>
+        <asp:Button ID="btnlogin" runat="server" Text="Button" type ="submit" name="" value="Login" OnClick="btnlogin_Click" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Mobile/Mlogin.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProjectWebForm.Models
+{
+    public partial class Mlogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            LoginProcess();
+        }
+
+        private void LoginProcess()
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString()))
+            {
+                con.Open();
+                //string query = @" SELECT COUNT(*) FROM CIS_USER WHERE USER_ID=@USER_ID AND USER_PWD=@USER_PWD ";
+                string query = @" SELECT COUNT(*) FROM CIS_BIZ_COMP WHERE CUSTOMER_CODE=@CUSTOMER_CODE AND CUSTOMER_PWD=@CUSTOMER_PWD ";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@CUSTOMER_CODE", txtCode.Text);
+                cmd.Parameters.AddWithValue("CUSTOMER_PWD", txtpwd.Text);
+                string output = cmd.ExecuteScalar().ToString();
+
+                // Login Success
+                if (output == "1")
+                {
+                    // create a session
+                    Session["user"] = txtCode.Text;
+                    Response.Redirect("../aspx/main.aspx");
+                }
+                // Login Fail
+                else
+                {
+                    Label1.Text = "Please Check your ID or Password!";
+                    txtCode.Text = "";
+                    txtpwd.Text = "";
+                }
+            }
+        }
+    }
+}
